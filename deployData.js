b5592bd1dcfcb42040117e0f1a532e2d0b66a4e6
Nafisaht <zahrotunnafisaht08@students.unnes.ac.id>
2023-06-28 10:54:14 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Zahrotun Nafisaht'; // TODO: replace this
+
+const studentId = 'FE4263861'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://magnificent-praline-35fab6.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
